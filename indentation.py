e1681f1cb40503425f43874d01c5e395f0e31bc6
Gill-G <gillg.dev@gmail.com>
2025-06-30 20:41:44 -0400

    added indentation.py

---
+++ indentation.py
@@ -0,0 +1,26 @@
+course = "Python 101"
+name = "Gurnoor"
+something = "something"
+something_else = "something else"
+
+if something == "something":
+    # our code can go in here
+    pass
+
+if something_else == "something else":
+    pass
+
+if something:
+    # do something else
+    if something_else:
+        # do another thing
+        pass
+    # write more stuff in here
+    pass
+
+
+# Test
+if name == "Gurnoor":
+    print(name)
+else:
+    print(course)
