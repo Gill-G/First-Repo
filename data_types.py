f84e22e98403d475bdfa42652372ec58b6b3fd4a
Gill-G <gillg.dev@gmail.com>
2025-06-30 21:17:03 -0400

    added data_types.py

---
+++ data_types.py
@@ -0,0 +1,30 @@
+# There are many different data/variable types
+# Such as: numbers, strings, lists, dictionaries, booleans, etc.
+# There are many more and you can even create your own
+
+string = 'A sentence' # can use apostrophes instead of quotes
+
+integers = 99 # number
+
+floats = 3.14 # a number with decimals
+
+list = ["Item 1", "Item 2", "Item 3"] # can also be called arrays
+
+list_2 = [string, integers, floats]
+
+tuple = ("Item 1", floats, integers) # its like a list but we cannot change it once its set
+
+set = {"Item 1", "Item 2", "Item 3"} # also like a list but its not going to maintain its order
+
+dictionary = {          # looks like an object?
+    "key": "value",
+    "key2": "value2",
+}
+
+boolean = True # either True or False
+
+none = None # nothing in the None data type
+
+
+#Test
+print(list_2)
